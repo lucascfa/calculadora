f4f50c5f2499aded16f7731e420cc4673bdfcad7
Lucas Carvalho <lucas.carvalhu.ferreira@gmail.com>
2018-10-27 18:36:26 -0300

    tentativa fail de implantar os deck

---
+++ app/src/main/java/com/example/admin/yougohcalculadora/Deck.java
@@ -0,0 +1,24 @@
+package com.example.admin.yougohcalculadora;
+
+import android.graphics.drawable.Drawable;
+
+public class Deck {
+    private String nome;
+    private int imagemDeck;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getImagemDeck() {
+        return imagemDeck;
+    }
+
+    public void setImagemDeck(int imagemDeck) {
+        this.imagemDeck = imagemDeck;
+    }
+}
